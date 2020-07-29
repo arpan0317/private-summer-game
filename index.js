8b24c648c9d674b0c752cabebf8f37d32eff28a8
Arpan gupta <arpan0317@yahoo.com>
2020-07-29 14:32:12 -0400

    no port

--- index.js
+++ index.js
@@ -12,21 +12,6 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
-
-
-
-
-var cloudant = new Cloudant({
-  account: '09a9e6fd-144f-4efd-9104-56dbbffae010-bluemix',
-  plugins: {
-    iamauth: {
-      iamApiKey: 'iQA27jc067HbbrD28szbErd-7dxmGjbhAYqXB0L4vIC5'
-    }
-  }
-});
 
 // set up mongoose
 
