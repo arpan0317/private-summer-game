24f08af58209a47ca667a416d00a4e9971bedbfd
Arpan gupta <arpan0317@yahoo.com>
2020-07-29 10:17:46 -0400

    change index

--- index.js
+++ index.js
@@ -33,6 +33,3 @@ mongoose.connect(
 // set up routes
 
 app.use("/users", require("./routes/userRouter"));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
