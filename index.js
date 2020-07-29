8f8dbc04a20d60d5a3c2fad74af0a4d75a17531f
Arpan gupta <arpan0317@yahoo.com>
2020-07-29 14:36:09 -0400

    env port

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
+
+
+
 
 // set up mongoose
 
