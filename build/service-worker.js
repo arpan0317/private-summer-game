b727e34008ddaa833dc8ac15f63bf9d865166131
Arpan gupta <arpan0317@yahoo.com>
2020-08-04 08:20:09 -0400

    new

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b303afab7867ddce7d5c68bd23456fcc.js"
+  "/precache-manifest.dbfcbd1cf5b41799e239e85042e650eb.js"
 );
 
 self.addEventListener('message', (event) => {
