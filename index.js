dbf21e9419c722dd99186381e4a6ed4d0d0709de
arpan0317 <arpan0317@yahoo.com>
2020-08-14 16:25:18 -0400

    testing final 8

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ mongoose.connect(
 // set up routes
 
 app.use("/users", require("./routes/userRouter"));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
